d6143d18f0af094f6831ea7559d9db246addc5ff
Marcos Manoel <marcosmss1304@gmail.com>
2019-06-09 23:08:01 -0300

    att bg

---
+++ src/components/ExpensesOcult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpensesOcult = (props) => {
+    return (
+        <div>
+            <h3 className="page-header__title">Not viewing 1 expense ocult</h3>
+        </div>
+    )
+};
+
+export default ExpensesOcult;
