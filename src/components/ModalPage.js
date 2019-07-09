d61826c2b103f00fbc8d21a1cd4a5438e41eeb92
Marcos Manoel <marcosmss1304@gmail.com>
2019-07-08 22:56:48 -0300

    box modal css configure

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
 class ModalPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            modalIsOpen: false,
-        };
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
 
-    openModal() {
-        this.setState({ modalIsOpen: true })
-    }
-    closeModal() {
-        this.setState({ modalIsOpen: false })
-    }
-    render() {
-        return (
-            <div>
-                <button className="button button--remove" onClick={this.openModal}>
-                    Remove Expense
-                </button>
-                <div className="modal__center">
-                    <Modal
-                        isOpen={this.state.modalIsOpen}
-                        onRequestClose={this.state.modalIsOpen}
-                        className="modal box modal__center"
-                    >
-                        <h2 className="page-header__title modal__title">Delete Expensify ?</h2>      
-                        <button className="button" onClick={this.props.removeExpense}>Yes</button>
-                        <button className="button" onClick={this.closeModal}>Close</button>
-                    </Modal>
-                </div>
-            </div>
-        )
-    }
-};
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+  render() {
+    return (
+      <div>
+        <button className="button button--remove" onClick={this.openModal}>
+          Remove Expense
+        </button>
+        <div className="modal__center">
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onRequestClose={this.state.modalIsOpen}
+            className="modal box modal__center"
+          >
+            <h2 className="page-header__title modal__title">
+              Delete Expensify ?
+            </h2>
+            <button className="button" onClick={this.props.removeExpense}>
+              Yes
+            </button>
+            <button className="button" onClick={this.closeModal}>
+              Not
+            </button>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ModalPage;
